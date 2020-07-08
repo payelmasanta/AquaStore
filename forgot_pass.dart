b8130ccd11790951135afda9d9eaaa175e435e07
yuktaatreya <44002986+yuktaatreya@users.noreply.github.com>
2020-07-08 21:46:07 +0530

    Create forgot_pass.dart

---
+++ forgot_pass.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './forgot_pass.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import './signup_page.dart';
+import 'dart:async';
+import './home_page.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white70,
+  fontFamily: 'OpenSans',
+  fontWeight: FontWeight.bold,
+  fontSize: 17,
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  fontFamily: 'OpenSans',
+);
+class ForgotPass extends StatefulWidget {
+  @override
+  _ForgotPassState createState() => _ForgotPassState();
+}
+class _ForgotPassState extends State<ForgotPass> {
+  String email;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF73AEF5),
+                  Color(0xFF61A4F1),
+                  Color(0xFF478DE0),
+                  Color(0xFF398AE5),
+                ],
+                stops: [0.1, 0.4, 0.7, 0.9],
+              ),
+            ),
+          ),
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 130,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 300,
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(''),
+                              Text(''),
+                              Text(''),
+                              Text(''),
+                              Text(" Confirm Email:", style: kLabelStyle),
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                validator: validateEmail,
+                                onSaved: (input) => email = input,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.only(top: 15),
+                                  prefixIcon:
+                                  Icon(Icons.email, color: Colors.white),
+                                  hintText: 'Enter your Email',
+                                  hintStyle: kHintTextStyle,
+                                ),
+                              ),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: double.infinity,
+                        margin: EdgeInsets.all(20),
+                        child: RaisedButton(
+                          elevation: 10,
+                          padding: EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+
+                          child: Text(
+                            'Submit',
+                            style: TextStyle(
+                                color: Color(0xFF527DAA),
+                                letterSpacing: 1.5,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'OpenSans'),
+                          ),
+                          onPressed: (){
+                            _formKey.currentState.save();
+                            resetPassword(email);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> resetPassword(String email) async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+    }
+  }
+
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+}
