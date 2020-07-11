2c10ece7f4e86efceb4680c530c34dadf2f528fc
payelmasanta <61454146+payelmasanta@users.noreply.github.com>
2020-07-11 16:10:22 +0530

    Update Calculation.dart

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -11,14 +11,21 @@ class Calculations extends StatefulWidget {
 }
 
 class _CalculationsState extends State<Calculations> {
-  Item selectedUser;
-  String roofsize;
+  Item selectedRegion, selectedCatchment;
+  double catchValue, roofsize, result;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  List<Item> users = <Item>[
+  List<Item> region = <Item>[
     const Item('North'),
     const Item('East'),
     const Item('South'),
     const Item('West'),
+  ];
+
+  List<Item> catchment = <Item>[
+    const Item('Tiles'),
+    const Item('Corrugated Metal Sheets'),
+    const Item('Concrete'),
+    const Item('Brick Pavement'),
   ];
 
   @override
@@ -45,99 +52,146 @@ class _CalculationsState extends State<Calculations> {
           ],
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.only(
-          top: 30,
-          left: 5,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Select Region',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            Text(""),
-            DropdownButton<Item>(
-              hint: Text("Select Region"),
-              value: selectedUser,
-              onChanged: (Item Value) {
-                setState(() {
-                  selectedUser = Value;
-                });
-              },
-              items: users.map((Item user) {
-                return DropdownMenuItem<Item>(
-                  value: user,
-                  child: Row(
-                    children: <Widget>[
-                      //user.icon,
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        user.name,
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 30,
+            left: 5,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Select Region',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              Text(""),
+              DropdownButton<Item>(
+                hint: Text("Select Region"),
+                value: selectedRegion,
+                onChanged: (Item Value) {
+                  setState(() {
+                    selectedRegion = Value;
+                  });
+                },
+                items: region.map((Item user) {
+                  //print(user.name);
+                  return DropdownMenuItem<Item>(
+                    value: user,
+                    child: Row(
+                      children: <Widget>[
+                        //user.icon,
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          user.name,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+              Text(""),
+              Text(
+                'Select City',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              Text(""),
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Enter the roof size (in m\u00B2):     ',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                   ),
-                );
-              }).toList(),
-            ),
-            Text(""),
-            Text(
-              'Select City',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            Text(""),
-            Row(
-              children: <Widget>[
-                Text(
-                  'Enter the roof size (in m\u00B2):     ',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
-                SizedBox(
-                  height: 37,
-                  width: 100,
-                  child: Form(
-                    key: _formKey,
-                    child: TextFormField(
-                      validator: (input) {
-                        //_formKey.currentState.save();
-                        if (input.isEmpty) {
-                          return 'Roofsize can not be blank';
-                        }
-                      },
-                      onSaved: (input) {
-                        roofsize = input;
-                        //_formKey.currentState.save();
-                      },
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(5),
-                        border: OutlineInputBorder(),
-                        //labelText: 'Roof size'),
+                  SizedBox(
+                    height: 37,
+                    width: 100,
+                    child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return 'Roofsize can not be blank';
+                          }
+                        },
+                        onSaved: (input) {
+                          roofsize = double.parse(input);
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(5),
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            RaisedButton(
-                child: Text('Submit'),
-                onPressed: () {
-                  _formKey.currentState.save();
-                  submitit(roofsize);
-                }),
-          ],
+                ],
+              ),
+              // catchment start
+              Text(""),
+              Text(
+                'Select Catchment Type:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              DropdownButton<Item>(
+                hint: Text("Select Catchment Type"),
+                value: selectedCatchment,
+                onChanged: (Item Value) {
+                  setState(() {
+                    selectedCatchment = Value;
+                    if (selectedCatchment.name == "Tiles") {
+                      catchValue = 0.85;
+                      //print(catchValue);
+                    } else if (selectedCatchment.name ==
+                        "Corrugated Metal Sheets") {
+                      catchValue = 0.8;
+                      //print(catchValue);
+                    } else if (selectedCatchment.name == "Concrete") {
+                      catchValue = 0.7;
+                      //print(catchValue);
+                    } else {
+                      catchValue = 0.55;
+                      //print(catchValue);
+                    }
+                  });
+                },
+                items: catchment.map((Item user) {
+                  return DropdownMenuItem<Item>(
+                    value: user,
+                    child: Row(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          user.name,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+              //catchment end
+              RaisedButton(
+                  child: Text('Submit'),
+                  onPressed: () {
+                    _formKey.currentState.save();
+                    submitit(roofsize, catchValue);
+                  }),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void submitit(String n) {
+  void submitit(double n, m) {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      print(n);
+      result = n * m;
+      print(result);
     }
   }
 }
