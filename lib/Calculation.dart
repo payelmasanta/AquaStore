3aa3b1b0c30b68b9543a0ebc66dcf5c8834b714b
Payel <61454146+payelmasanta@users.noreply.github.com>
2020-07-28 16:36:27 +0530

    Update Calculation.dart

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -1,8 +1,9 @@
-//import 'package:cloud_firestore/cloud_firestore.dart';//
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-//import 'package:rwh_assistant/database.dart';//
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:rwh_assistant/database.dart';
 
 class Item {
   const Item(this.name);
@@ -18,12 +19,15 @@ class _CalculationsState extends State<Calculations> {
   Future<void> _initForm;
   final _stateList = <StateModel>[];
   final _cityList = <City>[];
-  double resrain_dry, resrain_wet,demand;
+  double resrain_dry, resrain_wet, demand;
   int people;
   StateModel selectedState;
   City selectedCity;
   Item selectedRegion, selectedCatchment;
-  double catchValue, roofsize, result_dry, result_wet; //catchValue is the runn-off factor
+  double catchValue,
+      roofsize,
+      result_dry,
+      result_wet; //catchValue is the runn-off factor
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   List<Item> catchment = <Item>[
@@ -57,12 +61,12 @@ class _CalculationsState extends State<Calculations> {
     if (id == "1")
       return [
         City("1", "Bangalore", 150.2, 110),
-        City("2", "Mysore", 148.1,110),
+        City("2", "Mysore", 148.1, 110),
       ];
     if (id == "2")
       return [
-        City("3", "Kolkata", 160.8,100),
-        City("4", "Kharagpur", 166.5,100),
+        City("3", "Kolkata", 160.8, 100),
+        City("4", "Kharagpur", 166.5, 100),
       ];
     return Iterable.empty();
   }
@@ -87,12 +91,13 @@ class _CalculationsState extends State<Calculations> {
   void onCitySelected(City selectedCity) {
     setState(() {
       this.selectedCity = selectedCity;
-      resrain_dry = selectedCity.dry_coeff;
-      resrain_wet = selectedCity.wet_coeff;
+      resrain_dry = selectedCity.dry_rain;
+      resrain_wet = selectedCity.wet_rain;
       print(resrain_dry);
       print(resrain_wet);
     });
   }
+
   void _showLoadingDialog() {
     showDialog(
       context: context,
@@ -110,25 +115,30 @@ class _CalculationsState extends State<Calculations> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-      Container(
-      margin: EdgeInsets.only(left:5, right:20),
-      child:Image.asset(
-        'asset/logo3.png',
-        fit: BoxFit.contain,
-        height: 50,
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 5, right: 20),
+              child: Image.asset(
+                'assets/images/logo.png',
+                fit: BoxFit.contain,
+                height: 50,
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.only(left: 0.0, right: 0.0),
+                child: Text(
+                  'Calculate Rainfall',
+                  style: TextStyle(fontFamily: 'Open Sans'),
+                )),
+          ],
+        ),
       ),
-    ), Container(
-    padding: const EdgeInsets.only(left:0.0,right:0.0), child: Text('Calculate Rainfall',style: TextStyle(fontFamily: 'Open Sans'),)
-    ),
-    ],
-    ),
-    ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(left:10, right: 00),
+          margin: EdgeInsets.only(left: 10, right: 00),
           padding: EdgeInsets.only(
             top: 30,
             left: 5,
@@ -137,11 +147,12 @@ class _CalculationsState extends State<Calculations> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal:0),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
                 child: Text(
-                'Select State and City',
-                style: TextStyle(fontSize:21, fontWeight: FontWeight.bold),
-              ),),
+                  'Select State and City',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                ),
+              ),
               SafeArea(
                 child: FutureBuilder<void>(
                   future: _initForm,
@@ -157,19 +168,22 @@ class _CalculationsState extends State<Calculations> {
               ),
               Text(""),
               Text(''),
-                  Container(
-                   width:300,
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal:0),
-                    child: Form(
-                      key: _formKey,
-                      child:Column( children: <Widget>[
-                        Text(
-                          'Enter the roof size (in m\u00B2):     ',
-                          style: TextStyle(fontSize:21, fontWeight: FontWeight.bold),
-                        ),
-                        TextFormField(
+              Container(
+                width: 300,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Enter the roof size (in m\u00B2):     ',
+                        style: TextStyle(
+                            fontSize: 21, fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
                         keyboardType: TextInputType.number,
-                        validator: (input){
+                        validator: (input) {
                           if (input.isEmpty) {
                             return ('Roofsize can not be blank');
                           }
@@ -183,109 +197,114 @@ class _CalculationsState extends State<Calculations> {
                         ),
                       ),
                       Text(''),
-                        Text(''),
+                      Text(''),
                       Text(
-                        'Number of People using the water',
-                        style: TextStyle(fontSize:21, fontWeight: FontWeight.bold,),
+                        'Number of People using the water:',
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                        TextFormField(
-                          keyboardType: TextInputType.number,
-                          validator: (input){
-                            if (input.isEmpty) {
-                              return ('This field can not be blank');
-                            }
-                          },
-                          onSaved: (input) {
-                            people = int.parse(input);
-                          },
-                          decoration: InputDecoration(
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return ('This field can not be blank');
+                          }
+                        },
+                        onSaved: (input) {
+                          people = int.parse(input);
+                        },
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(5),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Text(''),
+                      Text(''),
+                      Text(
+                        'Water demand per person:',
+                        style: TextStyle(
+                            fontSize: 21, fontWeight: FontWeight.bold),
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        validator: (input) {
+                          if (input.isEmpty) {
+                            return ('This field  can not be blank');
+                          }
+                        },
+                        onSaved: (input) {
+                          demand = double.parse(input);
+                        },
+                        decoration: InputDecoration(
                             contentPadding: EdgeInsets.all(5),
                             border: OutlineInputBorder(),
-                          ),
-                        ),
-                        Text(''),
-                        Text(''),
-                        Text(
-                          'Water demand per person',
-                          style: TextStyle(fontSize:21, fontWeight: FontWeight.bold),
-                        ),
-                        TextFormField(
-                          keyboardType: TextInputType.number,
-                          validator: (input){
-                            if (input.isEmpty) {
-                              return ('This field  can not be blank');
-                            }
-                          },
-                          onSaved: (input) {
-                            demand = double.parse(input);
-                          },
-                          decoration: InputDecoration(
-                              contentPadding: EdgeInsets.all(5),
-                              border: OutlineInputBorder(),
-                              hintText: "20 (liters per day per person"
-                          ),
-                        ),
-                      ],
+                            hintText: "(Eg: 20 liters per day per person)"),
                       ),
-                    ),
-
+                    ],
                   ),
+                ),
+              ),
               // catchment start
-              Text("The minimum water demand according to the World Health Organization (WHO) is "
+              Text(
+                  "The minimum water demand according to the World Health Organization (WHO) is "
                   "20 litres per day. In semi-arid areas people often use less than 20 liters per person per day"),
               Text(""),
               Text(''),
               Text(''),
               Container(
                 //padding: EdgeInsets.only(left:10, right:0),
-                child:Text(
+                child: Text(
                   'Select Catchment Type:',
-                style: TextStyle(fontSize:21,fontWeight: FontWeight.bold),
-              ),),
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                ),
+              ),
               Container(
                 //padding: EdgeInsets.only(left:10, right:0),
-                child:DropdownButton<Item>(
+                child: DropdownButton<Item>(
                   hint: Text("Select Catchment Type"),
-                value: selectedCatchment,
-                onChanged: (Item Value) {
-                  setState(() {
-                    selectedCatchment = Value;
-                    if (selectedCatchment.name == "Tiles") {
-                      catchValue = 0.85;
-                      //print(catchValue);
-                    } else if (selectedCatchment.name ==
-                        "Corrugated Metal Sheets") {
-                      catchValue = 0.8;
-                      //print(catchValue);
-                    } else if (selectedCatchment.name == "Concrete") {
-                      catchValue = 0.7;
-                      //print(catchValue);
-                    } else {
-                      catchValue = 0.55;
-                      //print(catchValue);
-                    }
-                  });
-                },
-                items: catchment.map((Item user) {
-                  return DropdownMenuItem<Item>(
-                    value: user,
-                    child: Row(
-                      children: <Widget>[
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          user.name,
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-              ),),
+                  value: selectedCatchment,
+                  onChanged: (Item Value) {
+                    setState(() {
+                      selectedCatchment = Value;
+                      if (selectedCatchment.name == "Tiles") {
+                        catchValue = 0.85;
+                        //print(catchValue);
+                      } else if (selectedCatchment.name ==
+                          "Corrugated Metal Sheets") {
+                        catchValue = 0.8;
+                        //print(catchValue);
+                      } else if (selectedCatchment.name == "Concrete") {
+                        catchValue = 0.7;
+                        //print(catchValue);
+                      } else {
+                        catchValue = 0.55;
+                        //print(catchValue);
+                      }
+                    });
+                  },
+                  items: catchment.map((Item user) {
+                    return DropdownMenuItem<Item>(
+                      value: user,
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            user.name,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
               //catchment end
               RaisedButton(
-                padding:EdgeInsets.only(left:0, right:0),
+                  padding: EdgeInsets.only(left: 0, right: 0),
                   color: Colors.amber,
                   child: Text(
                     'Submit',
@@ -293,7 +312,7 @@ class _CalculationsState extends State<Calculations> {
                   ),
                   onPressed: () {
                     _formKey.currentState.save();
-                    submitit(roofsize, catchValue, resrain_dry,resrain_wet);
+                    submitit(roofsize, catchValue, resrain_dry, resrain_wet);
                   }),
             ],
           ),
@@ -326,64 +345,76 @@ class _CalculationsState extends State<Calculations> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Container(
-          width:150,
+          width: 150,
           //padding: EdgeInsets.only(left:10, right:0),
-          child:DropdownButtonFormField<StateModel>(
-                 hint: Text('Choose State'),
-          items: _stateList
-              .map((itm) =>
-              DropdownMenuItem(
-                value: itm,
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                        itm.name,
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
-                ),
-              )
-          ).toList(),
-          value: selectedState,
-          onChanged: _onStateSelected,
-        ),),
-          Container(
-            width:150,
-            //padding: EdgeInsets.only(left:10, right:0),
-            child: DropdownButtonFormField<City>(
-                hint: Text('Choose City'),
-          items: _cityList
-              .map((itm) =>
-              DropdownMenuItem(
-                child: Text(itm.name),
-                value: itm,
-              ))
-              .toList(),
-          value: selectedCity,
-          onChanged: onCitySelected,
-        ),)
+          child: DropdownButtonFormField<StateModel>(
+            hint: Text('Choose State'),
+            items: _stateList
+                .map((itm) => DropdownMenuItem(
+                      value: itm,
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            itm.name,
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList(),
+            value: selectedState,
+            onChanged: _onStateSelected,
+          ),
+        ),
+        Container(
+          width: 150,
+          //padding: EdgeInsets.only(left:10, right:0),
+          child: DropdownButtonFormField<City>(
+            hint: Text('Choose City'),
+            items: _cityList
+                .map((itm) => DropdownMenuItem(
+                      child: Text(itm.name),
+                      value: itm,
+                    ))
+                .toList(),
+            value: selectedCity,
+            onChanged: onCitySelected,
+          ),
+        )
       ],
     );
   }
 
-  void submitit(double n, m,d,w) async {
+  void submitit(double n, m, d, w) async {
     final FirebaseUser user = await FirebaseAuth.instance.currentUser();
     final String usr = user.uid.toString();
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      result_dry = (n*m)*d;
-      result_wet = (n*m)*w;
+      result_dry = (n * m) * d;
+      result_wet = (n * m) * w;
       print(result_dry);
       print(result_wet);
-
     }
+
+    DatabaseService(uid: usr)
+        .updateUserData(result_dry.toString(), result_wet.toString());
+    getData();
+  }
+
+  Future<DocumentSnapshot> getData() async {
+    var firebaseUser = await FirebaseAuth.instance.currentUser();
+    var resul = await Firestore.instance
+        .collection("result")
+        .document(firebaseUser.uid)
+        .get();
+    print(resul.data);
   }
 }
 
- class StateModel {
+class StateModel {
   final String id;
   final String name;
 
@@ -393,10 +424,8 @@ class _CalculationsState extends State<Calculations> {
 class City {
   final String id;
   final String name;
-  final double dry_coeff;
-  final double wet_coeff ;
+  final double dry_rain;
+  final double wet_rain;
 
-
-  City(this.id, this.name, this.dry_coeff, this.wet_coeff);
+  City(this.id, this.name, this.dry_rain, this.wet_rain);
 }
-
